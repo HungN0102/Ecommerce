0b9aa8aa652f00e9a932023379e2959e09b5bbb9
Hung Nguyen <b.hungng21@gmail.com>
2023-05-18 00:20:25 +0100

    base completed

--- ecommerce/store/views.py
+++ ecommerce/store/views.py
@@ -1,5 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Product, Category
 
 # Create your views here.
 def store(request):
-    return render(request, 'store/store.html')
+    all_products = Product.objects.all()
+    context_dict = {
+        'my_products': all_products
+    }
+    return render(request, 'store/store.html', context_dict)
+
+def categories(request):
+    all_categories = Category.objects.all()
+    return {'all_categories': all_categories}
+
+def product_info(request, slug):
+    product = get_object_or_404(Product, slug=slug)
+    context = {
+        'product': product 
+    }
+    
+    return render(request, 'store/product_info.html', context)
+
+def list_category(request, slug=None):
+    category = get_object_or_404(Category, slug=slug)
+    products = Product.objects.filter(category=category)
+    
+    return render(request, 'store/list_category.html',{
+        'category': category,
+        'products': products
+    })
