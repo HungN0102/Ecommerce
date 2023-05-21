2c5396d3eb5103878e662383b02c273f5bf70597
Hung Nguyen <b.hungng21@gmail.com>
2023-05-21 18:09:54 +0100

    update delete function

--- ecommerce/cart/views.py
+++ ecommerce/cart/views.py
@@ -4,26 +4,43 @@ from store.models import Product, Category
 from django.shortcuts import get_object_or_404
 from django.http import JsonResponse
 # Create your views here.
+
 def cart_summary(request):
-    return render(request, 'cart/cart_summary.html', {})
+
+    cart = Cart(request)
+
+    return render(request, 'cart/cart_summary.html', {'cart':cart})
 
 def cart_add(request):
     cart = Cart(request)
     
     if request.POST.get('action') == 'post':
-        product_id = int(request.POST.get('product_id'))
+        product_id = request.POST.get('product_id')
         product_quantity = int(request.POST.get('product_quantity'))
-        product = get_object_or_404(Product, product_id)
+        product = get_object_or_404(Product, id=product_id)
         
         cart.add(product=product, product_qty=product_quantity)
         response = JsonResponse({
-            'The product title is called: ': product.title,
-            '  and the product quantity is:  ': product_quantity
+            'qty': cart.count()
         })
         return response 
-
+    
 def cart_delete(request):
-    pass 
+    cart = Cart(request)
+    
+    if request.POST.get('action') == 'post':
+        product_id = request.POST.get('product_id')
+        cart.delete(product_id=product_id)
+        
+        response = JsonResponse({
+            'qty': cart.count(),
+            'total':cart.get_total()
+        })
+
+    return response 
 
 def cart_update(request):
-    pass 
+    cart = Cart(request)
+    if request.POST.get('action') == 'post':
+        product_id = request.POST.get('product_id')
+        cart.update()
