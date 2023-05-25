7c680d7fb5745ec221f801f765a65f86ac5bfd1f
Hung Nguyen <b.hungng21@gmail.com>
2023-05-25 22:31:42 +0100

    for login

--- ecommerce/account/urls.py
+++ ecommerce/account/urls.py
@@ -5,8 +5,14 @@ urlpatterns = [
     path('register', views.register, name='register'),
     
     # email verification urls 
-    path('email_verification<str:uidb64>/<str:token>', views.email_verification, name='email_verification'),
-    path('email_verification_sent', views.email_verification_sent, name='email_verification_sent'),
-    path('email_verification_success', views.email_verification_success, name='email_verification_success'),
-    path('email_verification_failed', views.email_verification_failed, name='email_verification_failed'),
+    path('email-verification<str:uidb64>/<str:token>', views.email_verification, name='email_verification'),
+    path('email-verification-sent', views.email_verification_sent, name='email_verification_sent'),
+    path('email-verification-success', views.email_verification_success, name='email_verification_success'),
+    path('email-verification-failed', views.email_verification_failed, name='email_verification_failed'),
+    
+    # login
+    path('my-login', views.my_login, name='my_login'),
+    
+    # dashboard
+    path('dashboard', views.dashboard, name='dashboard'),
 ]
--- ecommerce/account/views.py
+++ ecommerce/account/views.py
@@ -1,7 +1,9 @@
+# for general
 from django.shortcuts import render, redirect 
-from .forms import CreateUserForm
+from .forms import CreateUserForm, LoginForm
 from django.contrib.auth.models import User
 
+# for registration
 from django.contrib.sites.shortcuts import get_current_site
 from .token import user_tokenizer_generate
 
@@ -9,6 +11,10 @@ from django.template.loader import render_to_string
 from django.core.mail import send_mail
 from django.utils.encoding import force_bytes, force_str    
 from django.utils.http import urlsafe_base64_decode, urlsafe_base64_encode
+
+# for login
+from django.contrib.auth.models import auth 
+from django.contrib.auth import authenticate, login, logout
 
 def register(request):
     form = CreateUserForm()
@@ -68,3 +74,29 @@ def email_verification_success(request):
 
 def email_verification_failed(request):
     return render(request, 'account/registration/email_verification_failed.html', context={})
+
+def my_login(request):
+    form = LoginForm()
+    if request.method == 'POST':
+        form = LoginForm(request, data=request.POST)
+        
+        if form.is_valid():
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+            
+            user = authenticate(request, username=username, password=password)
+            
+            if user is not None:
+                auth.login(request, user)
+                return redirect('dashboard')
+            
+    context_dict = {
+        'form': form 
+    }
+    
+    return render(request, 'account/my_login.html',context=context_dict)
+
+def dashboard(request):
+    return render(request, 'account/dashboard.html', context={
+        
+    })
