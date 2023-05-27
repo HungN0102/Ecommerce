847536655a3d00ccc98bb8130b1976dca62513ea
Hung Nguyen <b.hungng21@gmail.com>
2023-05-27 02:02:52 +0100

    email validation

--- ecommerce/account/forms.py
+++ ecommerce/account/forms.py
@@ -42,3 +42,12 @@ class UpdateUserForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super(UpdateUserForm, self).__init__(*args, **kwargs)
         self.fields['email'].required = True
+        
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        if User.objects.filter(email=email).exclude(pk=self.instance.pk).exists():
+            raise forms.ValidationError('This email is used!')
+        if len(email) >= 350:
+            raise forms.ValidationError('This email is too long!')
+        
+        return email
