018f1723508b0aafeecb07f21b1ee5190a0d51dd
Hung Nguyen <b.hungng21@gmail.com>
2023-05-27 12:38:19 +0100

    password reset urls

--- ecommerce/account/urls.py
+++ ecommerce/account/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path 
 from . import views 
+from django.contrib.auth import views as auth_views 
 
 urlpatterns = [
     path('register', views.register, name='register'),
@@ -24,4 +25,11 @@ urlpatterns = [
     path('profile-management', views.profile_management, name='profile_management'),
     path('manage-shipping', views.manage_shipping, name='manage_shipping'),
     path('delete-account', views.delete_account, name='delete_account'),
+    
+    # password management views
+    path('reset-password', auth_views.PasswordResetView.as_view(), name='reset_password'), #submit our email form
+    path('reset-password-sent', auth_views.PasswordResetDoneView.as_view(), name='reset_password_done'), #submit our email form
+    path('reset/<uidb64>/<token>', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+    
 ]
