f9de5cc70643dd30577ef591bea8adda4070ed20
Hung Nguyen <b.hungng21@gmail.com>
2023-05-28 01:32:25 +0100

    creating shipping models

--- ecommerce/payment/views.py
+++ ecommerce/payment/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+# for general
+from django.shortcuts import render, redirect 
+
+# for login
+from django.contrib.auth.models import auth 
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+
 
 # Create your views here.
+def payment_success(request):
+    
+    return render(request, 'payment/payment_success.html')
+
+def payment_failed(request):
+    
+    return render(request, 'payment/payment_failed.html')
